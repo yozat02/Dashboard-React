dfa020cae6dfa69b812eb1b73173b0f3ed4ce887
youssefziat02 <youssefziat02@gmail.com>
2020-08-11 20:05:20 +0100

    inial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-redux-registration-login-example-ssoyvi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-redux-registration-login-example-ssoyvi)
---
+++ src/LoginPage/LoginPage.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // reset login status
+        this.props.logout();
+
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+        if (username && password) {
+            this.props.login(username, password);
+        }
+    }
+
+    render() {
+        const { loggingIn } = this.props;
+        const { username, password, submitted } = this.state;
+        return (
+            <div class=" content" >
+            <div class=" row" style={{margin:0}}>
+              <div class=" col-lg-8">
+              <img  src={"https://thegooddatafactory.com/images/logo-footer.png"} style={{width: 360,margin : 40}} />
+        <h6 style={{margin:60}}>Welcome </h6>
+        <p style={{margin:60}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed id ipsum sit amet metus mattis cursus. Nullam tristique, 
+          nibh ut tempus tristique, ligula sem mollis felis, ac luctus nunc purus consectetur sapien. 
+          Morbi in nunc pulvinar, scelerisque enim nec, lobortis enim. Aenean non tempor risus. 
+          Phasellus vestibulum dapibus libero, quis tristique dui faucibus ac. Etiam lacinia, odio at fermentum volutpat,
+           velit metus interdum erat, eu dapibus neque nisl sit amet est. Nam et est ut ipsum pulvinar efficitur vel non massa.
+            In gravida nec enim eget consequat. Suspendisse feugiat elit lacus, a posuere ligula convallis in. Duis laoreet
+             justo et fermentum suscipit, magna purus egestas ex, vitae elementum arcu dolor sit amet sem.</p>
+              </div>
+              <div class=" col-lg-4" >
+              <div style={{marginTop:250,backgroundColor:'#84ffff',borderRadius : 20,padding:15,width:350,borderStyle: "solid"}}>
+                <h2 style={{textAlign:"center"}}>Login</h2>
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                        {submitted && !username &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                        {submitted && !password &&
+                            <div className="help-block">Password is required</div>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Login</button>
+                        {loggingIn &&
+                            <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                        }
+                    </div>
+                </form>
+            </div>
+            <p>You don't have an account ?<Link to="/register" className="btn btn-link">Sign Up </Link></p>
+              </div>
+            </div>
+            
+          </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { loggingIn } = state.authentication;
+    return { loggingIn };
+}
+
+const actionCreators = {
+    login: userActions.login,
+    logout: userActions.logout
+};
+
+const connectedLoginPage = connect(mapState, actionCreators)(LoginPage);
+export { connectedLoginPage as LoginPage };
