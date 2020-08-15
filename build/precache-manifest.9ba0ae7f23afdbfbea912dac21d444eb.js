76f3168b17ce01f7f86a9e224c2fda23987a9635
youssefziat02 <youssefziat02@gmail.com>
2020-08-15 13:21:05 +0100

    Update detail page

---
+++ build/precache-manifest.9ba0ae7f23afdbfbea912dac21d444eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178e6faebed41f8c2bcfb82a4639a3be",
+    "url": "/Dashboard-React/index.html"
+  },
+  {
+    "revision": "3190c633aaf1094e1a8c",
+    "url": "/Dashboard-React/static/js/2.8dc7d180.chunk.js"
+  },
+  {
+    "revision": "4e4e8b39ecf9882f4c4281fd5207264d",
+    "url": "/Dashboard-React/static/js/2.8dc7d180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5e7971b6703869e50e",
+    "url": "/Dashboard-React/static/js/main.25090bea.chunk.js"
+  },
+  {
+    "revision": "3b04fc6637045a122d82",
+    "url": "/Dashboard-React/static/js/runtime-main.aedca2a8.js"
+  }
+]);
