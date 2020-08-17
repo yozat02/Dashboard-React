03c410ee085ee4deb86fa8268cf671d9f3e586e0
youssefziat02 <youssefziat02@gmail.com>
2020-08-17 05:42:11 +0100

    Update deploy

--- src/HomePage/HomePageBar.js
+++ src/HomePage/HomePageBar.js
@@ -15,7 +15,7 @@ export const HomePageBar = ({user}) => {
     const name = user.firstName.substring(0,1).toUpperCase()+user.lastName.substring(0,1).toUpperCase()
     return (
     <div style={{textAlign : "center",border: "1px solid",height:50}}>
-        <a href="/">
+        <a href="/Dashboard-React">
         <img src={"https://thegooddatafactory.com/images/logo-footer.png"} style={{width: 110,margin : 10,float:"left"}} />
         </a>
         <h4  style={{marginTop:15,marginRight:120,display:"inline-block" }}>Dashboad</h4>
